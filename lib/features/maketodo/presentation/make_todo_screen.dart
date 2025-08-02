2b75c8ca6708a365e01d0f138fcb7fdc5ee521d2
majid <majid.gha3mi.p@gmail.com>
2025-08-02 18:03:27 +0330

    maketodoscreen created

---
+++ lib/features/maketodo/presentation/make_todo_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+    
+class MakeTodoScreen extends StatelessWidget {
+
+  const MakeTodoScreen({ super.key });
+  
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(),
+    );
+  }
+}
