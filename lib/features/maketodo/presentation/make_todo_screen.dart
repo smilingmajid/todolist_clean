5eebe1aeea0f4e92650eea02dc2cfa72909195a2
majid <majid.gha3mi.p@gmail.com>
2025-08-02 19:18:33 +0330

    textfiedls and botton add to make_todo_screen

--- lib/features/maketodo/presentation/make_todo_screen.dart
+++ lib/features/maketodo/presentation/make_todo_screen.dart
@@ -2,17 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'controller/make_todo_controller.dart';
 
-class MakeTodoScreen extends GetView <MakeTodoController>{
+class MakeTodoScreen extends GetView<MakeTodoController> {
+  const MakeTodoScreen({super.key});
 
-  const MakeTodoScreen({ super.key });
-  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(''),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(),
+            SizedBox(height: 20),
+            TextField(),
+            SizedBox(height: 20),
+            ElevatedButton(onPressed: () {}, child: Text("Create ToDo")),
+          ],
+        ),
       ),
-      body: Container(),
     );
   }
 }
