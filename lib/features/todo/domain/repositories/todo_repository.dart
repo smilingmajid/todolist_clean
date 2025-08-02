7c3ea53f6cf3c2b0966a3fc34fadf321506d72de
majid <majid.gha3mi.p@gmail.com>
2025-08-02 18:00:28 +0330

    deleted todo directory and maktodo and todolist created

--- lib/features/todo/domain/repositories/todo_repository.dart
+++
@@ -1,7 +0,0 @@
-import '../entities/todo_entity.dart';
-
-abstract class TodoRepository {
-  Future<List<TodoEntity>> getTodos();
-  Future<void> addTodo(TodoEntity todo);
-  Future<void> deleteTodo(String id);
-}
