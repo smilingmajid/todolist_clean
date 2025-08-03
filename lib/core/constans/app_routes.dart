2814cf8c51635293746819e88f647e3a94b1fb9f
majid <majid.gha3mi.p@gmail.com>
2025-08-03 23:10:27 +0330

    TodoListScreen add to lib/core/constans/app_routes.dart

--- lib/core/constans/app_routes.dart
+++ lib/core/constans/app_routes.dart
@@ -1,7 +1,7 @@
 
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:todolist_clean/features/maketodo/presentation/binding/make_todo_bindings.dart';
+import 'package:todolist_clean/features/todolist/presentation/screens/todo_list_screen.dart';
 
 import '../../features/maketodo/presentation/make_todo_screen.dart';
 class AppRoutes {
@@ -19,7 +19,7 @@ GetPage(
 ),
   GetPage(
     name: AppRoutes.todoList,
-    page: ()=>Container(),
+    page: ()=>TodoListScreen(),
   binding: null,
   )
 
