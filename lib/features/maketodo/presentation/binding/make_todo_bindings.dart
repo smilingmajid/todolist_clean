a403d1d0a29ffd9f6d8683ad03bc8f9aa5d2ad34
majid <majid.gha3mi.p@gmail.com>
2025-08-04 10:05:32 +0330

    Dependencies Add  to  lib/features/maketodo/presentation/binding/make_todo_bindings.dart

--- lib/features/maketodo/presentation/binding/make_todo_bindings.dart
+++ lib/features/maketodo/presentation/binding/make_todo_bindings.dart
@@ -1,9 +1,22 @@
 import 'package:get/get.dart';
-import 'package:todolist_clean/features/maketodo/presentation/controller/make_todo_controller.dart';
+import 'package:hive/hive.dart';
+
+import '../../../maketodo/data/datasources/make_todo_local_datasource.dart';
+import '../../../maketodo/data/models/new_todo_model.dart';
+import '../../../maketodo/data/repositories/make_todo_repository_impl.dart';
+
+import '../../domin/usecases/create_todo_usecase.dart';
+import '../controller/make_todo_controller.dart';
 
 class MakeTodoBindings extends Bindings {
   @override
   void dependencies() {
-    Get.lazyPut<MakeTodoController>(() => MakeTodoController());
+    final box = Hive.box<NewTodoModel>('todos');
+    final localDataSource = MakeTodoLocalDataSource(box);
+    final repository = MakeTodoRepositoryImpl(localDataSource);
+    final createTodoUseCase = CreateTodoUseCase(repository);
+    Get.lazyPut<MakeTodoController>(
+      () => MakeTodoController(createTodoUseCase),
+    );
   }
 }
