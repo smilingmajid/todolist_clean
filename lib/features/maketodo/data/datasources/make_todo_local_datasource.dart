2c108cc1bf189087c855d55aefdefa837b2d205f
majid <majid.gha3mi.p@gmail.com>
2025-08-04 15:05:24 +0330

    Edit lib/features/maketodo/data/datasources/make_todo_local_datasource.dart

--- lib/features/maketodo/data/datasources/make_todo_local_datasource.dart
+++ lib/features/maketodo/data/datasources/make_todo_local_datasource.dart
@@ -1,12 +1,20 @@
 import 'package:hive/hive.dart';
 import '../models/new_todo_model.dart';
 
-class MakeTodoLocalDataSource {
-  final Box<NewTodoModel> box;
+class NewTodoLocalDataSource {
+  final Box<NewTodoModel> todoBox;
 
-  MakeTodoLocalDataSource(this.box);
+  NewTodoLocalDataSource(this.todoBox);
 
   Future<void> addTodo(NewTodoModel todo) async {
-    await box.put(todo.id, todo);
+    await todoBox.put(todo.id, todo);
+  }
+
+  List<NewTodoModel> getAllTodos() {
+    return todoBox.values.toList();
+  }
+
+  Future<void> deleteTodo(String id) async {
+    await todoBox.delete(id);
   }
 }
