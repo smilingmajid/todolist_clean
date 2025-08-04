1c5ad9b126c6b7860a019dadc7c86da2ae863fe4
majid <majid.gha3mi.p@gmail.com>
2025-08-04 14:45:28 +0330

    Edit lib/features/maketodo/domin/entities/new_todo.dart

--- lib/features/maketodo/domin/entities/new_todo.dart
+++ lib/features/maketodo/domin/entities/new_todo.dart
@@ -2,5 +2,8 @@ class NewTodo {
   final String title;
   final String description;
 
-  NewTodo({required this.title, required this.description});
+  NewTodo({
+    required this.title,
+    required this.description,
+  });
 }
